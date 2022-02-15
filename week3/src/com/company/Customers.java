828b681139f9e641fdcf078070591c2e93db608f
b410asakura <b410asakura@gmail.com>
2022-02-15 15:21:37 +0600

    add week 3

---
+++ week3/src/com/company/Customers.java
@@ -0,0 +1,80 @@
+package com.company;
+
+public class Customers {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String city;
+    private String country;
+
+    public Customers() {
+    }
+
+    public Customers(int customerId, String firstName, String lastName, String phoneNumber, String email, String city, String country) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
